a31760a0d3c1634c4e71119ebba434a0df99cfc1
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-03-10 11:18:05 +0300

    Add more endpoints

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
 import pandas as pd
-import matplotlib
-import numpy
 
 app = Flask(__name__)
 
@@ -14,8 +12,8 @@ def home():
     return render_template('home.html', data=stations.to_html())
 
 
-@app.route('/api/v1/<station>/<date>')
-def api(station, date):
+@app.route('/api/v1/<station>/<date>', methods=['GET'])
+def get_temp_by_station_id_and_date(station, date):
     filename = "data/TG_STAID" + str(station.zfill(6) + ".txt")
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
@@ -26,6 +24,25 @@ def api(station, date):
             "date": date,
             "temperature": temperature
         }
+
+
+@app.route('/api/v1/<station>', methods=['GET'])
+def get_temp_list_by_station_id(station):
+    filename = "data/TG_STAID" + str(station.zfill(6) + ".txt")
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route('/api/v1/annual/<station>/<year>', methods=['GET'])
+def get_temp_by_station_id_and_year(station, year):
+    filename = "data/TG_STAID" + str(station.zfill(6) + ".txt")
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[
+        df['    DATE'].str.startswith(str(year))
+    ].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
